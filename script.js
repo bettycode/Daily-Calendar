1b650fbc0be2254fc10f5e4992861b6b87bb4285
Bethlehem Balcha <betty.haile.us@gmail.com>
2020-10-11 22:07:14 -0400

    added some line of code

--- script.js
+++ script.js
@@ -1,70 +1,69 @@
-
-//set time and date using moment.js 
+//set time and date using moment.js
 var hour = moment().hours();
 
-function today(){
-    $("#currentDay").text(moment().format('LLLL'));
+function today() {
+    $("#currentDay").text(moment().format("LLLL"));
 }
-console.log(moment().format('LLLL'));
+console.log(moment().format("LLLL"));
 
 today();
 
 // color of the row based on present past and future.
-function rowColor(){
-    $(".event").each(function(){
+function rowColor() {
+    $(".event").each(function () {
         var t = parseInt($(this).attr("id"));
-        console.log(t)
+        console.log(t);
         var thour = parseInt(t);
-        console.log(thour)
-        if (thour === hour){
+        console.log(thour);
+        if (thour === hour) {
             $(this).addClass("present");
             console.log(this);
-        }else if(thour< hour){
+            
+        } else if (thour < hour) {
             $(this).addClass("past");
             console.log(this);
-        }else{
+        } else {
             $(this).addClass("future");
             console.log(this);
         }
-
     });
-};
+}
 
 rowColor();
 // function to get item from input
-function store(){
-    $(".event").each(function(){
+function store() {
+    $(".event").each(function () {
         var inputId = $(this).attr("id");
         $(this).val(localStorage.getItem(inputId));
     });
-};
-  store();
+}
+store();
 
 // add event listener to the save button. //setItem text
-  $(".saveBtn").on("click",function(){
+$(".saveBtn").on("click", function () {
     var schedule = $(this).siblings(".event").val();
     console.log(schedule);
     var inputs = $(this).siblings(".event").attr("id");
     console.log(inputs);
 
-    localStorage.setItem(inputs,schedule);
-    
+    localStorage.setItem(inputs, schedule);
 });
-
+// when hover change the text to good morning .
 $("#clear").hover(
-    function() {
+    function () {
         var $this = $(this); // caching $(this)
-        $this.data('defaultText', $this.text());
+        $this.data("defaultText", $this.text());
         $this.text("GOOD MORNING!");
     },
-    function() {
+
+    function () {
         var $this = $(this); // caching $(this)
-        $this.text($this.data('defaultText'));
+        $this.text($this.data("defaultText"));
     }
 );
 
 // event listener to clear the page and the text are.
-$("#clear").on("click",function(){
+$("#clear").on("click", function () {
     window.localStorage.clear();
     store() = "";
-})
+});
